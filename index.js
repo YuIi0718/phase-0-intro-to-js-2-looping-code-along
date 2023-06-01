b285be82730a143184b09e36402e0fe2d31f3db6
Weiyi Shen <vicky18058128335@outlook.com>
2023-05-31 21:20:54 -0400

    loop hw

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const nameList = ["Guadalupe", "Ollie", "Aki"];
+const describe = 'surprise';
+
+function writeCards(nameList, describe) {
+    const sentenceArray = [];
+    for (let i = 0; i < nameList.length; i++) {
+        sentenceArray.push(`Thank you, ${nameList[i]}, for the wonderful ${describe} gift!`);
+    }
+    return sentenceArray;
+}
+
+const countN = 5;
+function countDown(countN) {
+    for (let i = countN; i >=0 ; i--) {
+        console.log(i);
+    }
+}
+
